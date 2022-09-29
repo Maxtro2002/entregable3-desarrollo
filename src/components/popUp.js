47cf395b6df8d9774f2d8894e42c88923f4d396f
Maxtro2002 <jspr200231@gmail.com>
2022-09-29 17:34:40 -0500

    Animaion de carga al cargar pagina

--- src/components/popUp.js
+++ src/components/popUp.js
@@ -26,29 +26,29 @@ function MyVerticallyCenteredModal(props) {
                 var slots1 = station.free_bikes;
                 var slots2 = station.empty_slots;
               }
-
                 return(
-                    
-                    <Card key = {station.id} bg="secondary" text="light">
-                    <Card.Body>
-                    <Card.Title>Estación </Card.Title>
-                        <Card.Title>{station.name}</Card.Title>
-                        <Card.Text>Última Actualización: {station.timestamp}</Card.Text>
-                        <Card.Text>Bicicicletas Libres: {station.free_bikes === null ? msg : station.free_bikes}</Card.Text>
-                        <Card.Text>Espacios Libres: {station.empty_slots === null ? msg : station.empty_slots}</Card.Text>
-                        <Card.Text>Cantida de espacios totales: {slots1+slots2}</Card.Text>
-                    </Card.Body>
-                    </Card>
-                        
+                  <Card key = {station.id}>
+                  <Card.Body>
+                  <Card.Title>Estación </Card.Title>
+                      <Card.Title>{station.name}</Card.Title>
+                      <Card.Text>Última Actualización: {station.timestamp}</Card.Text>
+                      <Card.Text>Bicicicletas Libres: {station.free_bikes === null ? msg : station.free_bikes}</Card.Text>
+                      <Card.Text>Espacios Libres: {station.empty_slots === null ? msg : station.empty_slots}</Card.Text>
+                      <Card.Text>Cantida de espacios totales: {slots1+slots2}</Card.Text>
+                  </Card.Body>
+                  </Card>        
                 )
             })}
         </div>
+        
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
       </Modal.Footer>
     </Modal>
   );
+
+  
 }
 
 export default MyVerticallyCenteredModal;
